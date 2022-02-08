32aa194feffbec9067b8a37bdc97244fcad0f1fb
GitHub Action <action@github.com>
2022-02-08 18:47:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/soarex16/intellijideainspectiondemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.soarex16.intellijideainspectiondemo.services
+
+import com.github.soarex16.intellijideainspectiondemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
